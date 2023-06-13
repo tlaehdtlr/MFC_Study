9fa9d7b25fe5315bbc1bcf265e5ad130d49fd9c2
dongsik <tlaehdtlr@gmail.com>
2023-06-13 13:40:04 +0900

    create CMyListBox class and define OnLbnDblclk handler

---
+++ MessageReflection/CMyListBox.cpp
@@ -0,0 +1,42 @@
+// CMyListBox.cpp : implementation file
+//
+
+#include "pch.h"
+#include "MessageReflection.h"
+#include "CMyListBox.h"
+
+
+// CMyListBox
+
+IMPLEMENT_DYNAMIC(CMyListBox, CListBox)
+
+CMyListBox::CMyListBox()
+{
+
+}
+
+CMyListBox::~CMyListBox()
+{
+}
+
+
+BEGIN_MESSAGE_MAP(CMyListBox, CListBox)
+	ON_CONTROL_REFLECT(LBN_DBLCLK, &CMyListBox::OnLbnDblclk)
+END_MESSAGE_MAP()
+
+
+
+// CMyListBox message handlers
+
+
+
+
+void CMyListBox::OnLbnDblclk()
+{
+	// TODO: Add your control notification handler code here
+
+	CString str;
+	int nIndex = GetCurSel();
+	GetText(nIndex, str);
+	MessageBox(str);
+}
